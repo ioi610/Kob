defcd279736c6b50c16f297d5b1d4d71db61dd55
zzw <zhaozhenwang58@gmail.com>
2025-08-17 19:22:10 +0800

    创建后端

---
+++ backend/src/main/java/org/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,29 @@
+package org.kob.backend.controller.pk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+
+    @RequestMapping("getbotinfo/")
+    public List<Map<String, String>> getBotInfo() {
+        List<Map<String, String>> list = new LinkedList<>();
+        Map<String, String> bot1 = new HashMap<>();
+        bot1.put("name", "tiger");
+        bot1.put("rating", "1500");
+        Map<String, String> bot2 = new HashMap<>();
+        bot2.put("name", "apple");
+        bot2.put("rating", "2000");
+        list.add(bot1);
+        list.add(bot2);
+        return list;
+    }
+}
